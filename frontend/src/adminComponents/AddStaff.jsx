279f8990403058781c2f1c3fa404d34b035c260c
Nikhil Kumar Tiwari <88082428+nikhilkumar011@users.noreply.github.com>
2026-02-08 14:54:42 +0530

    Update AddStaff.jsx

--- frontend/src/adminComponents/AddStaff.jsx
+++ frontend/src/adminComponents/AddStaff.jsx
@@ -54,7 +54,7 @@ const AddStaff = () => {
               }, 2000);
               return;
             }
-            const res = await fetch("http://localhost:3000/user/signup", {
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/user/signup`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email: email, password: password, role: role, name:name })
