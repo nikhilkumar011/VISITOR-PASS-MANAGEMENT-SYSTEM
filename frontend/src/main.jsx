449083006543c280e9384873db80a729c819424b
Nikhil Kumar Tiwari <88082428+nikhilkumar011@users.noreply.github.com>
2026-02-08 15:33:50 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthContextProvider } from './context/authContext.jsx'
+import { AuthContextProvider } from './context/authcontext.jsx'
 
 
 
